a0ed88b6fdf13d5b7e8054ecacc1c038a693a9ff
Saito-Shogo-1234 <310Gosho@gmail.com>
2026-01-18 09:19:29 +0900

    Spring Boot プロジェクã初期構築

--- src/main/java/com/example/light_novel_management/LightNovelManagementApplication.java
+++ src/main/java/com/example/light_novel_management/LightNovelManagementApplication.java
@@ -1,13 +1,21 @@
 package com.example.light_novel_management;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LightNovelManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LightNovelManagementApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello() {
+		return "おはよう";
+	}
 }
